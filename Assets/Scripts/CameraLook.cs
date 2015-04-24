445d8ce015c8b595ce25dc86f49fb96698afaf38
lds56 <chenrui.qingqing@gmail.com>
2015-04-24 12:03:46 +0800

    First commit

---
+++ Assets/Scripts/CameraLook.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraLook : MonoBehaviour {
+
+	public float sensitivity = 1f;
+
+	public float minimumY = -60f;
+	public float maximumY = 60f;
+
+	private float rotationY = 0f;
+	private float rotationX = 0f;
+
+	void Update() {
+		rotationX = transform.localEulerAngles.y + Input.GetAxis ("Mouse X") * sensitivity;
+
+		rotationY += Input.GetAxis ("Mouse Y") * sensitivity;
+		rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+
+		transform.localEulerAngles = new Vector3 (-rotationY, rotationX, 0);
+
+	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float speed;
+	public float jumpSpeed;
+	public float gravity;
+	public float sensitivity;
+
+	private Vector3 moveDirection = Vector3.zero;
+	private bool moveStatus = false;
+	//private float prevMoveY = 0;
+
+	void Update() {
+
+		CharacterController controller = GetComponent<CharacterController>();
+
+		if (Input.GetMouseButtonDown (0)) {
+			Debug.Log("Mouse down");
+			moveStatus = !moveStatus;
+		}
+
+		float curSpeed = moveStatus? speed : 0;
+		controller.Move (Camera.main.transform.forward * 0.01f * curSpeed);
+
+		Debug.Log("Forward: " + Camera.main.transform.forward);
+
+	}
+
+
+}
