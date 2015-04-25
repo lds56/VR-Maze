c19c30a18ba27348e3c4b0644aa643309fbddad5
XYHHH <xuyhhh@gmail.com>
2015-04-25 15:22:59 +0800

    Integrate Google Cardboard SDK

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,31 +7,34 @@ public class PlayerController : MonoBehaviour {
 	//public float jumpSpeed;
 	public float gravity;
 	//public float sensitivity;
-
+	private GameObject head;
 	private Vector3 moveDirection = Vector3.zero;
 	private bool moveStatus = false;
 	//private float prevMoveY = 0;
+	void Start(){
+		head=GameObject.Find("Head");
+	}
 
 	void Update() {
 
 		CharacterController controller = GetComponent<CharacterController>();
 
 		if (Input.GetMouseButtonDown (0)) {
-			//Debug.Log("Mouse down");
+			Debug.Log("Mouse down");
 			moveStatus = !moveStatus;
 		}
 
 		float curSpeed = moveStatus? speed : 0;
-		moveDirection = Camera.main.transform.forward * 0.01f * curSpeed;
+		moveDirection = head.transform.forward * 0.01f * curSpeed;
 		moveDirection.y -= gravity;
 		controller.Move (moveDirection);
 
-		// Debug.Log("Forward: " + Camera.main.transform.forward);
+		Debug.Log("Forward: " + head.transform.forward);
 
 	}
 
-	void OnTriggerEnter(Collider other) {
+	//void OnTriggerEnter(Collider other) {
 		//Debug.Log ("Collide");
-		moveStatus = false;
-	}
+	//	moveStatus = false;
+	//}
 }
