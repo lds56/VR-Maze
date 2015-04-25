ae1b79a18f566a4d93631988fff8dc0f056865ad
XYHHH <xuyhhh@gmail.com>
2015-04-26 00:54:02 +0800

    version 1.0

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,20 +10,20 @@ public class PlayerController : MonoBehaviour {
 	private GameObject head;
 	private Vector3 moveDirection = Vector3.zero;
 	private bool moveStatus = false;
+	private CharacterController controller;
 	//private float prevMoveY = 0;
 	void Start(){
 		head=GameObject.Find("Head");
+		controller = GetComponent<CharacterController>();
 	}
 
 	void Update() {
-
-		CharacterController controller = GetComponent<CharacterController>();
-
-		/*if (Input.GetMouseButtonDown (0)) {
+	/*
+		if (Input.GetMouseButtonDown (0)) {
 			Debug.Log("Mouse down");
 			moveStatus = !moveStatus;
-		}*/
-
+		}
+*/
 		float curSpeed = moveStatus? speed : 0;
 		moveDirection = head.transform.forward * 0.01f * curSpeed;
 		moveDirection.y -= gravity;
@@ -32,10 +32,17 @@ public class PlayerController : MonoBehaviour {
 		//Debug.Log("Forward: " + head.transform.forward);
 
 	}
+	/*
+	public void SetGazedAt(bool gazedAt) {
+		GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
+		Debug.Log ("gaze!");
+	}
 
+*/
 	public void moveOrStop()
 	{
 		moveStatus = !moveStatus;
+		//Debug.Log ("click!");
 	}
 
 	//void OnTriggerEnter(Collider other) {
