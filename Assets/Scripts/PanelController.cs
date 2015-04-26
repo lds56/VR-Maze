8a43767a247a749b4c6dda9f517aaa86bceebb85
XYHHH <xuyhhh@gmail.com>
2015-04-27 00:21:32 +0800

    version 1.1

---
+++ Assets/Scripts/PanelController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelController : MonoBehaviour {
+	private GameObject wall;
+	private GameObject light;
+	private GameObject ground;
+	void Start()
+	{
+		light= GameObject.Find ("Point light");
+		wall = GameObject.Find ("Wall");
+		ground=GameObject.Find ("GROUND");
+	}
+
+	public void Replay()
+	{
+		DontDestroyOnLoad (wall);
+		DontDestroyOnLoad (ground);
+		DontDestroyOnLoad (light);
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}
